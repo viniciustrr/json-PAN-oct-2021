92932cef51dcb6ee448197ee586db7e2e7f32db9
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-28 21:14:52 -0300

    Json manipulation with java

---
+++ src/main/java/com/vinicius/app/JsonApplication.java
@@ -0,0 +1,34 @@
+package com.vinicius.app;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.simple.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonApplication {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		SpringApplication.run(JsonApplication.class, args);
+		
+		JSONObject jsonzinho = new JSONObject();
+		jsonzinho.put("Nome", "Julio");
+		jsonzinho.put("Sobrenome", "Smith");
+		jsonzinho.put("idade", 25);
+		jsonzinho.put("Nome", "Julio");
+		jsonzinho.put("Sobrenome", "Smith");
+		jsonzinho.put("idade", 25);
+		
+		System.out.println(jsonzinho);
+		PrintWriter salvar = new PrintWriter("./src/main/java/teste2.json");
+		salvar.write(jsonzinho.toJSONString());
+		salvar.flush();
+		salvar.close();
+		
+		
+		
+	}
+
+}
