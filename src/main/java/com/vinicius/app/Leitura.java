ee389384fdaf0f47e9858436cc9bb2b38ef0f05b
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-31 21:59:43 -0300

    Atividade json

--- src/main/java/com/vinicius/app/Leitura.java
+++ src/main/java/com/vinicius/app/Leitura.java
@@ -2,33 +2,55 @@ package com.vinicius.app;
 
 import java.io.FileNotFoundException;
 
-//Java program to read JSON from a file
+
 
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Leitura {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+	public static void main(String[] args) throws FileNotFoundException {
+        String caminhoDoArquivo = "./src/main/java/";
+        String nomeDoArquivo = "teste4.json";
+        String caminhoArquivoCompleto = caminhoDoArquivo+nomeDoArquivo;
 
-		Object obj = new JSONParser().parse(new FileReader("./src/main/java/teste2.json"));
+        try {
+            leMultiplosObjetosDoJSON(caminhoArquivoCompleto);
+        } catch (IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
 
-		JSONObject jsonzinho = (JSONObject) obj;
+    }
+	
+	
+	 private static void leMultiplosObjetosDoJSON(String caminhoArquivo) throws IOException, ParseException {
+	        JSONArray jsonArray = (JSONArray)  new JSONParser().parse(new FileReader(caminhoArquivo));
 
-		// get nome
-		String nominho = (String) jsonzinho.get("Nome");
-		String sobrenominho = (String) jsonzinho.get("Sobrenome");
-		long idade = (long) jsonzinho.get("idade");
-		
-		System.out.println(nominho);
-		System.out.println(sobrenominho);
-		System.out.println(idade);
+	        Iterator<JSONObject> iteratorObjetoJSON = jsonArray.iterator();
 
+	        while(iteratorObjetoJSON.hasNext()) {
+	            JSONObject objetoJSONAtual = (JSONObject) iteratorObjetoJSON.next();
 
-	}
+	            Set<Map.Entry<String, String>> atributos = objetoJSONAtual.entrySet();
+	            for(Map.Entry<String, String> atributo : atributos) {
+	                System.out.println(atributo.getKey() + "  -----  " + atributo.getValue());
+	            }
+
+	            System.out.println();
+	            System.out.println();
+	        }
+	    }
 }
+
+
+
+
